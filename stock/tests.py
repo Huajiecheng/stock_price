6829136fdf756d22733f8c03b1dc290f45f0b4f1
Huajiecheng <chenghuajie1998@gmail.com>
2021-06-04 07:20:39 -0400

    implement post request and add django test

--- stock/tests.py
+++ stock/tests.py
@@ -1,3 +1,77 @@
 from django.test import TestCase
+from stock.models import Stock, StockPrice
+from django.shortcuts import render, get_object_or_404
+from django.core import serializers
+from django.db.models import Q
+import datetime
+import json
+from django.test import TestCase, Client
+from django.http import HttpResponse, Http404, JsonResponse
+from decimal import Decimal
 
-# Create your tests here.
+client = Client()
+class StockTest(TestCase):
+
+    def setUp(self):
+        stocks = Stock.objects.create(symbol='MSFT')
+        stocks.save()
+        StockPrice.objects.create(
+            time = datetime.datetime.strptime("2020-01-13", '%Y-%m-%d'), stock = stocks, open_price = 1.1, high_price = 1.1, low_price = 1.1, close_price = 1.1
+        )
+        StockPrice.objects.create(
+            time = datetime.datetime.strptime("2020-01-14", '%Y-%m-%d'), stock = stocks, open_price = 2.1, high_price = 2.1, low_price = 2.1, close_price = 2.1
+        )
+        StockPrice.objects.create(
+            time = datetime.datetime.strptime("2020-01-15", '%Y-%m-%d'), stock = stocks, open_price = 3.1, high_price = 3.1, low_price = 3.1, close_price = 3.1
+        )
+    
+    def test_get(self):
+        response = client.get("/stock/get_stockdata?symbol=MSFT&start=2020-01-13&end=2020-01-14")
+        json_data = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers["Content-Type"], "application/json")
+        self.assertEqual(len(json_data), 2)
+
+    def test_delete(self):
+        response = client.get("/stock/delete_stockdata?symbol=MSFT")
+        self.assertEqual(response.status_code, 204)
+
+    def test_add(self):
+        stockdata = [{'time':'2020-01-16', 'stock':'MSFT', 'open_price':4.1, 'high_price' : 4.1, 'low_price' : 4.1, 'close_price' : 4.1},
+           {'time':'2020-01-13', 'stock':'AAPL', 'open_price':4.1, 'high_price' : 4.1, 'low_price' : 4.1, 'close_price' : 4.1}]
+        response = client.post(
+            "/stock/add_stockdata",
+            data=json.dumps({"data":stockdata}),
+            content_type='application/json'
+        )       
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(len(Stock.objects.all()), 2)
+        self.assertEqual(len(StockPrice.objects.all()), 5)
+
+    def test_update(self):
+        stockdata = [{'time':'2020-01-15', 'stock':'MSFT', 'open_price':4.1, 'high_price' : 4.1, 'low_price' : 4.1, 'close_price' : 4.1}]
+        response = client.post(
+            "/stock/add_stockdata",
+            data=json.dumps({"data":stockdata}),
+            content_type='application/json'
+        )
+        updatetime = datetime.datetime.strptime('2020-01-15', '%Y-%m-%d')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(len(Stock.objects.all()), 1)
+        self.assertEqual(len(StockPrice.objects.all()), 3)
+        self.assertEqual(StockPrice.objects.get(time = updatetime).open_price, Decimal('4.10'))
+
+    def test_get_error(self):
+        response = client.get("/stock/get_stockdata?symbol=MSFT&start=2020-01-13")
+        json_data = json.loads(response.content)
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_error(self):
+        response = client.get("/stock/delete_stockdata?symbol=AAPL")
+        self.assertEqual(response.status_code, 404)
+
+
+    
+        
+
+    
--- stock/views.py
+++ stock/views.py
@@ -9,37 +9,48 @@ from asgiref.sync import sync_to_async
 
 # add or update stock data
 def add_data(request):
-	if request.method == "GET":
-		if Stock.objects.filter(symbol ="NN").exists():
-			stocks = Stock.objects.get(symbol = "NN")
-		else:
-			stocks = Stock(symbol = "NN")
-			stocks.save()
-		prices = StockPrice(time = datetime.datetime.strptime("2020-01-13", '%Y-%m-%d'), stock = stocks, open_price = 1.1, high_price = 1.1, low_price = 1.1, close_price = 1.1)
-		prices.save()
-		return HttpResponse("Successully add stock data")
+	if request.method == "POST":
+		json_data = json.loads(request.body)['data']		
+		for record in json_data:
+			if Stock.objects.filter(symbol = record['stock']).exists():
+				stocks = Stock.objects.get(symbol = record['stock'])
+			else:
+				stocks = Stock(symbol = record['stock'])
+				stocks.save()
+			try:
+				record_time = datetime.datetime.strptime(record['time'], '%Y-%m-%d')
+			except ValueError:
+				return HttpResponse("Wrong time format",status=404)
+			if StockPrice.objects.filter(Q(stock=stocks) & Q(time=record_time)).exists():
+				StockPrice.objects.filter(Q(stock=stocks) & Q(time=record_time)).update(open_price = float(record['open_price']), 
+					high_price = float(record['high_price']), low_price = float(record['low_price']), close_price = float(record['close_price']))
+			else:
+				prices = StockPrice(time = record_time, stock = stocks, open_price = float(record['open_price']), 
+					high_price = float(record['high_price']), low_price = float(record['low_price']), close_price = float(record['close_price']))
+				prices.save()
+		return HttpResponse("Successully add stock data", status = 201)
 
 # get json data based on symbol and time range
 # example: http://127.0.0.1:8000/stock/get_stockdata?symbol=NN&start=2020-01-13&end=2020-01-14
-async def get_data(request):
+def get_data(request):
 	if request.method == "GET":
 		if 'symbol' in request.GET:
 			item = get_object_or_404(Stock, symbol=request.GET["symbol"])
 		else:
-			return HttpResponse("Missing symbol parameter")
-
+			return HttpResponse("Missing symbol parameter", status=404)
 		if 'start' in request.GET and 'end' in request.GET:
 			try:
 				start_time = datetime.datetime.strptime(request.GET["start"], '%Y-%m-%d')
 				end_time = datetime.datetime.strptime(request.GET["end"], '%Y-%m-%d')
 			except ValueError:
-				return HttpResponse("Wrong time format")
+				return HttpResponse("Wrong time format",status=404)
+			item = get_object_or_404(Stock, symbol=request.GET["symbol"])
 			data = StockPrice.objects.filter(Q(stock=item) & Q(time__range=[start_time, end_time])).order_by("time")
 			context = serializers.serialize("json", data) 
 			context = json.loads(context)
 			return JsonResponse(context, safe = False)
 		else:
-			return HttpResponse("Missing time range parameter")
+			return HttpResponse("Missing time range parameter",status=404)
 
 #delete stock and related data based on symbol
 def delete_data(request):
@@ -47,6 +58,6 @@ def delete_data(request):
 		if 'symbol' in request.GET:
 			item = get_object_or_404(Stock, symbol=request.GET["symbol"])
 			item.delete()
-			return HttpResponse("successully delete")
+			return HttpResponse("successully delete", status =204)
 		else:
-			return HttpResponse("No symbol parameter")
+			return HttpResponse("No symbol parameter",status=404)
